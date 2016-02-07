944a13da744c574ab546417a9ffed086b715b038
Romb Evgeniy <e.o.romb@gmail.com>
2016-02-07 12:32:19 +0600

    1_0_0_0

---
+++ CompositeKeyDictionray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositeKeyDictionray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompositeKeyDictionary<int, int, string> dict = new CompositeKeyDictionary<int, int, string>();
+            dict.Add(5, 5, "Hello");
+            string res;
+            dict.TryGetValue(5, 5, out res);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
